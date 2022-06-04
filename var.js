7aaf69570677673ccd53d1462f8d1e515e93b9b8
sebastian carballlo <sebastiancarballo.sc.sc@gmail.com>
2022-06-04 01:25:30 -0300

    Agregue estructura principal html  a variables.html

--- var.js
+++ var.js
@@ -2,3 +2,5 @@
 //Una variable en javascript es un espacio en la memoria de la pc, donde podemos almacenar datos.
 //(Primitivos:string,number,boolean,null,undefined,Nan)
 //(Compuestos: objetos{},arrays[],functions(),Class{},etc)
+
+//Declaracion de variables
