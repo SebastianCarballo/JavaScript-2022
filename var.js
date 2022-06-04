7bf12f395de313949a539fc314c44d9475ea7149
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-06-04 01:04:55 -0300

    agregue notas a var.js

---
+++ var.js
@@ -0,0 +1,4 @@
+//VARIABLES
+//Una variable en javascript es un espacio en la memoria de la pc, donde podemos almacenar datos.
+//(Primitivos:string,number,boolean,null,undefined,Nan)
+//(Compuestos: objetos{},arrays[],functions(),Class{},etc)
