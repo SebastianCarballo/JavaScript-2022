a00f591d0233aa0db6355c70a9ff0fb4f794a24c
sebastian carballlo <sebastiancarballo.sc.sc@gmail.com>
2022-06-04 09:45:24 -0300

    cree archivo let.jsl

---
+++ variables/let.js
@@ -0,0 +1,26 @@
+//LET =>
+
+/* 
+let
+
+La instrucción let declara una variable de alcance local con ámbito de bloque(block scope), la cual, opcionalmente, puede ser inicializada con algún valor.
+
+Sintaxis
+
+let var1 [= valor1] [, var2 [= valor2]] [, ..., varN [= valorN]];
+
+*/
+
+let nombre = "sebastian";
+let edad = 27;
+let hijos = "si";
+
+
+console.log("=============== Variable let ===================");
+console.log(nombre,edad,hijos);
+console.log(nombre);
+console.log(edad);
+console.log(hijos);
+
+
+//let puede ser útil para escribir código más limpio cuando usamos funciones internas.
