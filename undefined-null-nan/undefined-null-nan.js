95cb9e96d1362b9a62a3162df9de3d3ff87ee88b
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-06-05 18:22:52 -0300

    cree archivo undefined-null-NaN

---
+++ undefined-null-nan/undefined-null-nan.js
@@ -0,0 +1,10 @@
+//null -undefined - NaN
+
+//Undefined representa un valor ausente es decir ausencia de valor
+//es una variable que no tiene valor, que no a sido inicializada
+
+//Null
+/* El valor null es un literal de Javascript que representa intencionalmente un valor nulo o "vacío". Es uno de los valores primitivos de Javascript. */
+
+//si no le asignamos valor es undefined
+let indefinida;
