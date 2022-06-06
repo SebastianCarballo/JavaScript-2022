b68e87c1d3f53809b13e892a6345255b614eb938
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-06-06 20:55:08 -0300

    cree archivo funciones.js

---
+++ funciones/funciones.js
@@ -0,0 +1,24 @@
+//Funciones
+//¿Que son las funciones?
+//Una funciones es simplemente un bloque de codigo autocontenido, es decir que tiene su propoio contenido, intependienta a el scopue o al ambito global.
+//Se definen 1 sola  vez y se pueden ejecutar en cualquier momento, es decir, es un pequeño fracmento que nosotros vamola declsarar una vez y reutilizar tantas veces como sea necesario
+//Una funcion puede o no recibir parametros, los parametros son los datos que rtecibe una funcion separados por comas.
+//una funcion tambien puede devolver avlores dependiendo cual sea el caso.
+//las funciones en javascript son un tipo de dato muy especial, tambien se consideran objetos.
+//Las funciones son muy poderosas en javascript por que las podemnos asignar como valor, podemos retornar la ejecucion de una funcion adicional 
+//desde ES6 tenemos muchas caracteristicas interesantes por ejemplo las funciones flechas o arrow functions
+
+//Tenemos dos tipos de funciones 
+//Las funciones declaradas y
+//Las funciones expresadas
+
+//Funcion Declarada
+//para declarar una funcion declarada utilizamos la palanbra reservada funtion nombre de la funcion () y {} y dentro de las llaves va el cuerpo de la funcion 
+
+function estoEsUnaFunction(){
+    console.log("Uno");
+    console.log("Dos");
+    console.log("Tres");
+    
+}
+//Esat funcion ya esta declarada esperando ser llamada
