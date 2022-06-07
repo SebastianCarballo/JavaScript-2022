c4c8192584a2aeabf8dfce6b92b6b846cd11ddf8
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-06-07 11:15:50 -0300

    modifique arrays.js

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -8,6 +8,59 @@ Los arrays son objetos similares a una lista cuyo prototipo proporciona métodos
  */
 
 //tenemos difrentes manera de declarar arrays
-
+//Un arreglo no es mas que una coleccion de elementos, que pueden ser de diferente tipo
 const a = [];
+const b = [1,true,"Hola",["A","B","C",[1,2,3]]];
 console.log(a);
+console.log(b);
+console.log(b[2]);
+console.log(b[0]);
+console.log(b[3][2]);
+console.log(b[3][3][0]);
+
+
+//Otra forma de crear arreglos
+
+const c = Array.of("X","Y","Z",9,8,7);
+console.log(c);
+
+//OTRA FOrma por ejemplo en tu ecommers tienes 1000 productos
+//Metodos contructor de arrays
+const d = Array(100).fill(false);
+console.log(d);
+
+//IMportante los arreglo van a empezar en la pocision numero 0
+
+//Practicas que ya no se usan
+
+const e = new Array();
+console.log(e);
+
+const f = new Array(1,2,3,true,false);
+console.log(f);
+
+//Los array tienen propñiedades y metodos
+//propiedades .length()
+
+const colores = ["Rojo","Verde","azul"];
+console.log(colores);
+//para agregar un nuevo elemento tenemos el metodo push
+//push siempre lo va poner al final
+colores.push("Negro");
+console.log(colores);
+
+//MEtodo .Pop()
+//Quita el ultimo elemento del array
+colores.pop()
+console.log(colores);
+
+///los arreglos tienen una gran cantidad de metodos
+
+//metodo for Each
+//Este metodo nos permite ejecutar una funcion por cada uno de los elementos que tenga el arreglo
+
+colores.forEach(function (el, index){
+    console.log(`<li id="${index}>${el}</li>`);
+});
+
+//con el metodo forEach podemos recorrer el valor con el primer parametro que generalmente se le ponenelement o el y el segundo parametro es el indice que ocupa cada elemento dentro del array
