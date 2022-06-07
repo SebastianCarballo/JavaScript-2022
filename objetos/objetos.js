6a96297075d73066e2c31d9baf088e629c6be747
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-06-07 11:31:36 -0300

    Cree archivo objetos.js y su html

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -1,1 +1,8 @@
 //Objetos
+//Son una parte muy importante de javascript 
+//por eso la frace en javascript todo es un objeto
+
+//ESta manera ya no se usa
+//Estamos utilizando el metodo constructor
+let a = new String();
+console.log(a);
