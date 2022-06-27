663939bb552698680806fff6972556b7ebd6c837
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-06-27 15:45:56 -0300

    Subo mas notas a CONDICIONALES

--- 10-condicionales/condicionales.js
+++ 10-condicionales/condicionales.js
@@ -79,3 +79,78 @@ if(hora >= 0 && hora <= 5){
 }else{
     console.log("Buenas noches");
 }
+
+if(hora > 0 && hora < 6){
+    console.log("Dejame dormir");
+}else if( hora > 5 && hora < 12){//anidamos if else
+    console.log("Buenos dias");
+}else if(hora > 11 && hora < 19){
+    console.log("Buenas tardes");
+}else{
+    console.log("Buenas noches");
+}
+
+
+//Simplificacion de un if-else
+//Operador Ternario
+//Como su operador lo dice tiene tres partes
+
+/* Operador Ternario
+Cuenta con una (condicion), que si esa condicion se cumple a verdadero sigue un signo de ? y la linea de codigo a ejutar si la condicion se cumple a verdadero: y luego la linea que quieras que se ejecute si la condion es falsa 
+
+Operador Ternario(condicion) ? verdadero: falsa
+
+En un operador ternario solo puedes ejucutar una linea de codigo
+
+*/
+
+console.log("Operador Ternario");
+
+let eresMayor = (edad >= 18)? "Eres mayor de edad":"Eres menor de edad";
+console.log(eresMayor);
+
+
+//Estructura de control switch
+//swith-case
+//Nos va servir cuando nosotros tengamos que evaluar diferentes situaciones o valores para una  misma variable
+
+/* 
+domingo-0
+lunes-1
+martes-2
+miercoles-3
+jueves-4
+viernes-5
+sabado-6
+domingo-7
+*/
+
+let dia =2;
+
+switch (dia) {
+    case 0://Casos = case   value = valor
+        console.log("Domingo");
+        break;
+    case 1:
+        console.log("Lunes");
+        break;
+    case 2:
+        console.log("Martes");
+        break;
+    case 3:
+        console.log("Miercoles");
+        break;
+    case 4:
+        console.log("Jueves");
+        break;
+    case 5:
+        console.log("Viernes");
+        break;
+    case 6:
+        console.log("Sabado");
+        break;
+
+    default:
+        console.log("El dia no existe");
+        break;
+}
